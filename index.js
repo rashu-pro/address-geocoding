e8470838141c0133f1ddadd258ff6a61f25fc007
rashu-pro <rashu.pro@gmail.com>
2023-11-20 14:15:19 +0600

    modified the code two make standalone function read and write data in async/await way

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 const fs = require('fs');
 const PapaParse = require('papaparse');
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyCRIIew-eQp2QjI5mRLFOE-qoUnl-qKC38'
+});
+
+const inputFilePath = "assets/sample-csv-file.csv";
+const outputFilePath = "assets/sample-csv-modified.csv";
 
 // Read a csv file
-const filePath = "assets/sample-csv-file.csv";
-
-fs.readFile(filePath, 'utf8', (err, data)=>{
-    if(err){
-        console.log('Error reading the file: ', err.message);
-        return;
+async function readCSVFile(filePath) {
+    try {
+        const data = await fs.promises.readFile(filePath, 'utf8');
+        return new Promise((resolve, reject) => {
+            PapaParse.parse(data, {
+                header: true,
+                dynamicTyping: true,
+                complete: (results) => {
+                    resolve(results.data);
+                },
+                error: (error) => {
+                    reject(error);
+                },
+            });
+        });
+    } catch (error) {
+        console.error('Error reading or parsing CSV file:', error.message);
+        throw error;
     }
+}
 
-    // Parse csv data
-    PapaParse.parse(data, {
-        header: true,
-        complete: (results) => {
-            const data = results.data;
-            console.log(data);
-        },
-        error: (err) => {
-            console.log('Error parsing csv: ', err.message);
-        }
-    })
-})
+// Write data as csv file
+async function writeCSVFile(outputFilePath, modifiedData) {
+    try {
+        const csvString = PapaParse.unparse(modifiedData, { header: true });
+        await fs.promises.writeFile(outputFilePath, csvString, 'utf8');
+        console.log('File written successfully:', outputFilePath);
+    } catch (error) {
+        console.error('Error writing CSV file:', error.message);
+        throw error;
+    }
+}
+
+// Process the data to write
+async function processData() {
+    try {
+        const parsedData = await readCSVFile(inputFilePath);
+
+        // Modify the parsed data (replace this with your own logic)
+        const modifiedData = modifyData(parsedData);
+
+        await writeCSVFile(outputFilePath, modifiedData);
+    } catch (error) {
+        console.error('Error processing data:', error.message);
+    }
+}
+
+// Modify the data
+function modifyData(data) {
+    // Your modification logic here
+    return data;
+}
+
+// Invoke the async function
+processData();
