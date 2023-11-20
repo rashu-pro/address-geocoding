29cabcbc74ebde277491e5d6b4b139e9a753bcb0
rashu-pro <rashu.pro@gmail.com>
2023-11-20 15:54:27 +0600

    initial script completed to format and geocoding the given address and saving it into a csv file

--- index.js
+++ index.js
@@ -4,8 +4,70 @@ const googleMapsClient = require('@google/maps').createClient({
     key: 'AIzaSyCRIIew-eQp2QjI5mRLFOE-qoUnl-qKC38'
 });
 
-const inputFilePath = "assets/sample-csv-file.csv";
-const outputFilePath = "assets/sample-csv-modified.csv";
+// Get the geocoded address using existing address and other parameters like zipcode, city, state
+async function getAddress(address, supportingAddressParameter, count) {
+    let parameter = supportingAddressParameter[count];
+    let addressQuery = address + ',' + parameter;
+    let formattedAddress = await geocodeAddress(addressQuery);
+    if (formattedAddress.length < 1) {
+        count = count + 1;
+        getAddress(address, supportingAddressParameter, count);
+        return;
+    }
+
+    return formattedAddress;
+}
+
+// Function to geocode an address
+function geocodeAddress(address) {
+    return new Promise((resolve, reject) => {
+        googleMapsClient.geocode({
+            address: address
+        }, (err, response) => {
+            if (!err) {
+                resolve(response.json.results);
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
+
+// Function to geocode addresses and create the csv file with the updated data
+function geocodeAddressesAndSaveToCsv(data) {
+    const promises = data.map(async (item) => {
+        const address = `${item["AddressLine1"]}, ${item["ZipCode"]}`;
+
+        // Geocode the address
+        // const response = await geocodeAddress(address);
+        let zipCode = String(item['ZipCode']);
+        if (zipCode.length < 5) {
+            zipCode = '0' + zipCode;
+        }
+        const addressParameter = [zipCode, item['City'], item['State']];
+        const response = await getAddress(item['AddressLine1'], addressParameter, 0);
+
+        if (response && response[0]) {
+            // Update latitude and longitude in the original data
+            item["Latitude"] = response[0].geometry.location.lat;
+            item["Longitude"] = response[0].geometry.location.lng;
+            // item["AddressLine1"] = response[0].formatted_address.replace(/, USA$/, '');
+            item["AddressLine1"] = response[0].formatted_address;
+        }
+
+        return item;
+    });
+
+    // Wait for all promises to complete
+    Promise.all(promises)
+        .then((updatedData) => {
+            // Save the updated data to a CSV file
+            writeCSVFile(outputFilePath, updatedData);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
 
 // Read a csv file
 async function readCSVFile(filePath) {
@@ -34,7 +96,7 @@ async function writeCSVFile(outputFilePath, modifiedData) {
     try {
         const csvString = PapaParse.unparse(modifiedData, { header: true });
         await fs.promises.writeFile(outputFilePath, csvString, 'utf8');
-        console.log('File written successfully:', outputFilePath);
+        console.log('File written successfully:', modifiedData);
     } catch (error) {
         console.error('Error writing CSV file:', error.message);
         throw error;
@@ -46,20 +108,14 @@ async function processData() {
     try {
         const parsedData = await readCSVFile(inputFilePath);
 
-        // Modify the parsed data (replace this with your own logic)
-        const modifiedData = modifyData(parsedData);
-
-        await writeCSVFile(outputFilePath, modifiedData);
+        // Geocode address and save into csv
+        geocodeAddressesAndSaveToCsv(parsedData);
     } catch (error) {
         console.error('Error processing data:', error.message);
     }
 }
 
-// Modify the data
-function modifyData(data) {
-    // Your modification logic here
-    return data;
-}
-
+const inputFilePath = "assets/person-addresses.csv";
+const outputFilePath = "assets/sample-csv-modified.csv";
 // Invoke the async function
 processData();
